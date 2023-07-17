b36270d5c6504329d1e3ba67491220194385cd17
Nebula <nebulaanish@gmail.com>
2023-07-17 20:43:00 +0545

    Updated solution to 017 maxConsecutiveOnes

--- 017 maxConsecutiveOnes.py
+++ 017 maxConsecutiveOnes.py
@@ -1,26 +1,18 @@
-# This code doesn't pass all the edge cases, Will rework on this problem tomorrow
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        i=0
+        j=0
+        ans = 0
+        zero = 0
 
-def func(nums,k):
-    i = 0
-    j = k
-    max = 0
-    numOfZero = 0
-    for itr in nums[:k]:
-        if itr==0:
-            numOfZero+=1
-    max = 0
-    while(j<len(nums) and i<j):
-        # print(i,j, numOfZero)
-        if numOfZero <=k:
+        while(j<len(nums)):
+            if nums[j]==0:
+                zero +=1 
+            while(zero>k):
+                if nums[i]==0:
+                    zero -=1
+                i+=1
             j+=1
-            if nums[j-1]==0:
-                numOfZero+=1
-            
-        if numOfZero >k:
-            if nums[i]==0:
-                numOfZero-=1
-            i+=1
-        if (j-i)>max:
-            max = j-i
-    print(max)
-    return max
+            ans = max(ans,j-i)
+        print(ans)
+        return ans
