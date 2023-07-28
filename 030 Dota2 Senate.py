9d6537de8928bfbc683ace3dd429613ce9c82424
Nebula <nebulaanish@gmail.com>
2023-07-28 23:50:22 +0545

    Solution 030

---
+++ 030 Dota2 Senate.py
@@ -0,0 +1,8 @@
+# DDRRR
+# DRRD  # D removes first R and goes to end of queue
+# RDD   # D removes second R and goes to end of queue
+# DR    # R removes D and goes to end
+# D     # D removes R
+        # D declares itself the winner
+    
+    
