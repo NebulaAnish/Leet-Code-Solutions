87b7276865776d3c478fd662f6231026b9cba684
Nebula <nebulaanish@gmail.com>
2023-07-29 23:51:49 +0545

    Solution 030

--- 030 Dota2 Senate.py
+++ 030 Dota2 Senate.py
@@ -5,4 +5,23 @@
 # D     # D removes R
         # D declares itself the winner
     
-    
+class Solution:
+    def predictPartyVictory(self, senate: str) -> str:
+        senate = list(senate)
+        Dqueue , Rqueue = deque(), deque()
+
+        for i, c in enumerate(senate):
+            if  c == "R":
+                Rqueue.append(i)
+            else:
+                Dqueue.append(i)
+        
+        while Dqueue and Rqueue:
+            dTurn = Dqueue.popleft()
+            rTurn = Rqueue.popleft()
+            if rTurn < dTurn:
+                Rqueue.append(dTurn+len(senate))
+            else:
+                Dqueue.append(rTurn+len(senate))
+
+        return "Radiant" if Rqueue else "Dire"
